32583b11b887a8c38f57606b69f276a03fd3d5c6
tj <dev@LouisetugansMBP.nycap.rr.com>
2018-03-28 23:04:33 -0400

    Added esimatingtool app

--- src/estimating/urls.py
+++ src/estimating/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from estimatingtools.views import *
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
 ]
