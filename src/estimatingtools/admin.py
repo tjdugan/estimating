23df757cb750f4cd0f1ebd9534ce458176310680
tj <dev@LouisetugansMBP.nycap.rr.com>
2018-04-24 13:41:27 -0400

    added content to database

--- src/estimatingtools/admin.py
+++ src/estimatingtools/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Materials)
+admin.site.register(ProductionRate)
+admin.site.register(Manufacturer)
+admin.site.register(ProductRep)
+admin.site.register(SalesRep)
+admin.site.register(ContractorRep)
+admin.site.register(Distributor)
+admin.site.register(Contractor)
+admin.site.register(Architect)
+admin.site.register(Equipment)
+admin.site.register(LaborRate)
